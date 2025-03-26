0544162c0a1e5a1477f77d3da00ae52161e76d15
Nick Reardon <nickjreardon@gmail.com>
2025-03-26 07:58:29 -0400

    progress on search

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,29 @@
-export default function bs_list(haystack: number[], needle: number): boolean {
+var log = console.log;
 
+export default function bs_list(haystack: number[], needle: number): boolean {
+    log(`----- Searching for needle: ${needle} -------`);
+    let idx_low = 0;
+    let idx_hi = haystack.length - 1;
+    do {
+        const idx_mid = idx_low + Math.floor((idx_hi - idx_low) / 2);
+        const val_mid = haystack[idx_mid];
+        log(`[low, high] :: mid --> [${haystack[idx_low]}, ${haystack[idx_hi]}] :: ${val_mid}`);
+      
+        if (needle === val_mid) {
+            // found the needle!
+            log('Returning true.');
+            return true;
+        } else if (needle < val_mid) {
+            // needle is smaller...inspect left half
+            idx_hi = idx_mid - 1; // left of mid becomes the new "high"
+            continue;
+        } else {
+            // needle is larger...inspect right half
+            idx_low = idx_mid + 1; // right of mid becomes the new "low"
+            continue;
+        }
+    } while (idx_low <= idx_hi);
+    // we've exhausted all options in the array without a match.
+    log('Returning false.')
+    return false;
 }
--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,29 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
+  
+  // This formula can be arrived at with calculus & discrete optimization...
+  // But the main thing is that it's BETTER than linear.
+  const jumpAmount = Math.floor(Math.sqrt(breaks.length));
 
+  // ---- TESTING FIRST CRYSTAL BALL ----
+  // test for breaks at each jump interval, beginning with the first one
+  let i = jumpAmount;
+  for (; i < breaks.length; i += jumpAmount) {
+    // exit this loop once it breaks
+    if (breaks[i]) {
+      break;
+    }
+  }
+
+  // ---- TESTING SECOND CRYSTAL BALL ----
+  // jump back to the previous interval where the ball didnt break
+  i -= jumpAmount;
+  // walk one floor at a time, testing until it breaks
+  // j represents the additional floor(s) being tested above i (the last safe jump interval)
+  for (let j = 0; j < jumpAmount && i < breaks.length; i++, j++) {
+    if (breaks[i]) {
+      return i
+    }
+  }
+
+  return -1
 }
