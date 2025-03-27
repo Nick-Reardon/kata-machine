cb2c8954853e8ee5ac5ab5245cb52c17d4c73921
Nick Reardon <nickjreardon@gmail.com>
2025-03-27 07:55:17 -0400

    bubble

--- src/__tests__/BubbleSort.ts
+++ src/__tests__/BubbleSort.ts
@@ -4,7 +4,11 @@ test("bubble-sort", function () {
     const arr = [9, 3, 7, 4, 69, 420, 42];
 
     debugger;
-    bubble_sort(arr);
-    expect(arr).toEqual([3, 4, 7, 9, 42, 69, 420]);
+    // bubble_sort(arr);
+    // expect(arr).toEqual([3, 4, 7, 9, 42, 69, 420]);
+
+    const arr2 = [9, 7, 4, 69, 420, 42, 3];
+    bubble_sort(arr2);
+    expect(arr2).toEqual([3, 4, 7, 9, 42, 69, 420]);
 });
 
--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -1,3 +1,16 @@
 export default function bubble_sort(arr: number[]): void {
-
+  
+  for (let i = 0; i < arr.length; i++) {
+    // -- NOTE --
+    // minus 1 accounts for the second num being compared
+    // minus i accounts for the growing portion at the end that is sorted
+    for (let j = 0; j < arr.length - i - 1; j++) {
+      
+      if (arr[j] > arr[j+1]) {
+        const tmp= arr[j];
+        arr[j] = arr[j+1];
+        arr[j+1] = tmp;
+      }
+    }
+  }
 }
